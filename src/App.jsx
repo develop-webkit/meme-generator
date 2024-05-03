54914f804ef7441e89badd3a5a8cbec10aafb9b8
Nabeel Yaseen <nabeelyaseenggwp@gmail.com>
2024-05-03 23:25:47 +0500

    1.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css';
 import Header from "./components/Header.jsx";
+import Main from "./components/Main.jsx";
 
 function App() {
 
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,1 +1,36 @@
-import 
+import memeIcon from "../assets/meme-icon.png";
+import memeData  from "../meme.js";
+
+export default function Main(){
+    let url = "url here";
+    function getImage(){
+        const memeArray = memeData.data.memes;
+        const getRandom = Number.parseInt(Math.random() * memeArray.length);
+        ({url} = memeArray[getRandom]);
+    };
+
+    return (
+        <>
+            <main>
+                <form action="">
+                    <p>{url}</p>
+                    <div className="form-label-block">
+                        <div className="form-label">
+                            <label htmlFor="topText">
+                                Top Text
+                                <input placeholder="Top Text" type="text" name="topText" id="topText" />
+                            </label>
+                        </div>
+                        <div className="form-label">
+                            <label htmlFor="bottomText">
+                                Bottom Text
+                                <input placeholder="Bottom Text" type="text" name="bottomText" id="bottomText" />
+                            </label>
+                        </div>
+                    </div>
+                    <button onClick={getImage} className="submitBtn" type="button">Get New Image <img src={memeIcon} alt="small picture frame" /></button>
+                </form>
+            </main>    
+        </>
+    )
+}
